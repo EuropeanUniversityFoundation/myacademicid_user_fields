4504313fdf7b91ee2267c8fe96362ac4a4990a52
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-12-10 12:25:02 +0100

    Assert that the fields are instances of BaseFieldDefinition

--- tests/src/Kernel/UserBaseFieldsTest.php
+++ tests/src/Kernel/UserBaseFieldsTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\myacademicid_user_fields\Kernel;
 
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
@@ -10,6 +11,15 @@ use Drupal\KernelTests\KernelTestBase;
  * @group myacademicid_user_fields
  */
 class UserBaseFieldsTest extends KernelTestBase {
+
+  /**
+   * Fields to test.
+   */
+  const NEW_BASE_FIELDS = [
+    'maid_schac_home_organization',
+    'maid_schac_personal_unique_code',
+    'maid_voperson_external_affilliation'
+  ];
 
   /**
    * Modules to enable.
@@ -32,8 +42,11 @@ class UserBaseFieldsTest extends KernelTestBase {
     $fields = \Drupal::service('entity_field.manager')
       ->getFieldDefinitions('user', 'user');
 
-    $this->assertArrayHasKey('maid_schac_home_organization', $fields);
-    $this->assertArrayHasKey('maid_schac_personal_unique_code', $fields);
-    $this->assertArrayHasKey('maid_voperson_external_affilliation', $fields);
+    foreach (self::NEW_BASE_FIELDS as $field) {
+      // Test whether the field exists.
+      $this->assertArrayHasKey($field, $fields);
+      // Test whether the field is a base field.
+      $this->assertInstanceOf(BaseFieldDefinition::class, $fields[$field]);
+    }
   }
 }
