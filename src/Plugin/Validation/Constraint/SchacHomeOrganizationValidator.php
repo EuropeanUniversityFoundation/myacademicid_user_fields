21bd015ec1c6d7645afb7be2953b169729632e6b
Tiago Simões <tiago.simoes@uni-foundation.eu>
2021-12-11 14:28:22 +0100

    Better handling of hyphens in hostnames; no RegEx left

--- src/Plugin/Validation/Constraint/SchacHomeOrganizationValidator.php
+++ src/Plugin/Validation/Constraint/SchacHomeOrganizationValidator.php
@@ -8,9 +8,9 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the SchacHomeOrganization constraint.
  *
- * This is essentially a long winded way to validate a domain name.
- * It could be done with a single RegEx found on StackExchange.
- * However, a readable explanation of that RegEx would be longer than this code.
+ * This is essentially a long winded way to validate a hostname.
+ * It could possibly be accomplished with a single RegEx found on StackExchange.
+ * However, a readable explanation of that RegEx might be longer than this code.
  */
 class SchacHomeOrganizationValidator extends ConstraintValidator {
 
@@ -32,40 +32,8 @@ class SchacHomeOrganizationValidator extends ConstraintValidator {
       );
 
       if (!$fail) {
-        // Validate individual domain name components.
-        $components = \explode('.', $item->value);
-
-        // The last component is the Top Level Domain (TLD).
-        $tld = $components[\count($components)-1];
-        // The TLD component must contain only letters.
-        // The longest known TLD is 24 characters long.
-        $fail = (!\ctype_alpha($tld) || \strlen($tld) > 24);
-
-        if (!$fail) {
-          // Validate all other components.
-          for ($i=0; $i < \count($components)-1; $i++) {
-            $component = $components[$i];
-
-            $fail = (
-              // No leading, trailing or consecutive dots allowed.
-              empty($component) ||
-              // Each component has a maximum length of 63 characters.
-              \strlen($component) > 63 ||
-              (
-                // A component can be only a single character long.
-                \strlen($component) === 1 &&
-                // A single character component must be alphanumerical.
-                \preg_match('/^[a-zA-Z0-9]$/', $component) !== 1
-              ) ||
-              // A component can contain alphanumerical characters and dashes.
-              // A component cannot start or end with a dash.
-              \preg_match(
-                '/^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]$/',
-                $component
-              ) !== 1
-            );
-          }
-        }
+        // Validate individual hostname labels.
+        $fail = validateLabels($item->value);
       }
 
       if ($fail) {
@@ -77,4 +45,74 @@ class SchacHomeOrganizationValidator extends ConstraintValidator {
     }
   }
 
+  /**
+   * Validates hostname labels.
+   *
+   * @param string $hostname
+   *
+   * @return bool $fail
+   */
+  private function validateLabels(string $hostname): bool {
+    $fail = FALSE;
+
+    $labels = \explode('.', $hostname);
+
+    // The last label is the Top Level Domain (TLD).
+    $tld = $labels[\count($labels)-1];
+    // The TLD label must contain only letters.
+    // The longest known TLD is 24 characters long.
+    $fail = (!\ctype_alpha($tld) || \strlen($tld) > 24);
+
+    if (!$fail) {
+      // Validate all other labels.
+      for ($i=0; $i < \count($labels)-1; $i++) {
+        $label = $labels[$i];
+
+        if (!$fail) {
+          // No leading, trailing or consecutive periods allowed.
+          // Each label has a maximum length of 63 characters.
+          $fail = (empty($label) || \strlen($label) > 63);
+        }
+
+        if (!$fail) {
+          // There are restrictions on the domain label in particular.
+          $domain = ($i === \count($labels)-2);
+          // Validate an individual label.
+          $fail = validateSingleLabel($label, $domain);
+        }
+      }
+    }
+
+    return $fail;
+  }
+
+  /**
+   * Validates a single label.
+   *
+   * @param string $label
+   * @param bool $domain
+   *
+   * @return bool $fail
+   */
+  private function validateSingleLabel(string $label, bool $domain): bool {
+    $fail = FALSE;
+
+    // Hyphens are allowed, under certain conditions.
+    $parts = \explode('-', $label);
+
+    // No leading or trailing hyphens allowed.
+    $fail = (
+      empty($parts[0]) ||
+      empty($parts[\count($parts)-1])
+    );
+
+    // In the particular case of a domain label, another rule applies.
+    if (!$fail && $domain) {
+      // A double hyphen cannot occur in the third and fourth positions.
+      $parts = \explode('--', $label);
+      $fail = (\count($parts) > 1 && \strlen($parts[0]) === 2);
+    }
+
+    return $fail;
+  }
 }
