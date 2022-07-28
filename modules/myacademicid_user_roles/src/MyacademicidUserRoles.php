337c062ea3713a81264891d2d950ffd87aac097a
Tiago Simões <tiago.simoes@uni-foundation.eu>
2022-07-28 02:52:16 +0200

    No Events needed, service called from hook: both modes work.

--- modules/myacademicid_user_roles/src/MyacademicidUserRoles.php
+++ modules/myacademicid_user_roles/src/MyacademicidUserRoles.php
@@ -6,14 +6,8 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
-use Drupal\user\Entity\Role;
-use Drupal\user\Entity\User;
 use Drupal\user\UserInterface;
-use Drupal\myacademicid_user_fields\Event\SetUserVopersonExternalAffilliationEvent;
 use Drupal\myacademicid_user_fields\MyacademicidUserFields;
-use Drupal\myacademicid_user_roles\Event\UserRoleChangeEvent;
-use Drupal\myacademicid_user_roles\Event\SetUserRolesEvent;
-use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * MyAcademicID User Roles service.
@@ -30,13 +24,6 @@ class MyacademicidUserRoles {
   protected $configFactory;
 
   /**
-   * Event dispatcher.
-   *
-   * @var \Symfony\Contracts\EventDispatcher\EventDispatcherInterface
-   */
-  protected $eventDispatcher;
-
-  /**
    * The messenger.
    *
    * @var \Drupal\Core\Messenger\MessengerInterface
@@ -48,8 +35,6 @@ class MyacademicidUserRoles {
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
-   * @param \Symfony\Contracts\EventDispatcher\EventDispatcherInterface $event_dispatcher
-   *   The event dispatcher service.
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger.
    * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
@@ -57,12 +42,10 @@ class MyacademicidUserRoles {
    */
   public function __construct(
     ConfigFactoryInterface $config_factory,
-    EventDispatcherInterface $event_dispatcher,
     MessengerInterface $messenger,
     TranslationInterface $string_translation
   ) {
     $this->configFactory     = $config_factory;
-    $this->eventDispatcher   = $event_dispatcher;
     $this->messenger         = $messenger;
     $this->stringTranslation = $string_translation;
   }
@@ -73,55 +56,106 @@ class MyacademicidUserRoles {
    * @param \Drupal\user\UserInterface $user
    */
   public function checkRoleChange(UserInterface $user) {
-    $old_roles = (empty($user->original)) ? NULL : $user->original
-      ->getRoles(TRUE);
-    $new_roles = $user
-      ->getRoles(TRUE);
+    $mode = $this->configFactory
+      ->get('myacademicid_user_fields.settings')
+      ->get('mode');
 
-    if ($old_roles !== $new_roles) {
-      // Instantiate our event.
-      $event = new UserRoleChangeEvent((string) $user->id());
-      // Dispatch the event.
-      $this->eventDispatcher
-        ->dispatch($event, UserRoleChangeEvent::EVENT_NAME);
+    switch ($mode) {
+      case MyacademicidUserFields::SERVER_MODE:
+        // Check for differences in user roles.
+        $old_roles = (empty($user->original)) ? NULL : $user->original
+          ->getRoles(TRUE);
+        $new_roles = $user
+          ->getRoles(TRUE);
+
+        // Check for differences in schac_home_organization.
+        $old_sho = (empty($user->original)) ? NULL : $user->original
+          ->get(MyacademicidUserFields::FIELD_SHO);
+        $new_sho = $user
+          ->get(MyacademicidUserFields::FIELD_SHO);
+
+        // Either difference calls for a recalculation of affilliation.
+        if ($old_roles !== $new_roles || $old_sho !== $new_sho) {
+          $sho = [];
+
+          foreach ($new_sho as $idx => $item) {
+            $sho[] = $item->value;
+          }
+
+          $this->affilliationFromRoles($user, $new_roles, $sho);
+        }
+
+        break;
+
+      default:
+        // Check for differences in voperson_external_affilliation.
+        $old_vea = (empty($user->original)) ? NULL : $user->original
+          ->get(MyacademicidUserFields::FIELD_VEA);
+        $new_vea = $user
+          ->get(MyacademicidUserFields::FIELD_VEA);
+
+        // A difference calls for a recalculation of user roles.
+        if ($old_vea !== $new_vea) {
+          $vea = [];
+
+          foreach ($new_vea as $idx => $item) {
+            $vea[] = $item->value;
+          }
+
+          $this->rolesFromAffilliation($user, $vea);
+        }
+
+        break;
     }
   }
 
   /**
    * Determine affilliation to assign based on roles and home organization.
    *
-   * @param string $uid
+   * @param \Drupal\user\UserInterface $user
    * @param array $roles
    * @param array $sho
    */
-  public function affilliationfromRoles(string $uid, array $roles, array $sho) {
-    $mode = $this->configFactory
-      ->get('myacademicid_user_fields.settings')
-      ->get('mode');
+  public function affilliationfromRoles(UserInterface $user, array $roles, array $sho) {
+    $role_mapping = $this->configFactory
+      ->get('myacademicid_user_roles.role_to_affilliation')
+      ->get('role_mapping');
 
-    if ($mode === MyacademicidUserFields::SERVER_MODE) {
-      $role_mapping = $this->configFactory
-        ->get('myacademicid_user_roles.role_to_affilliation')
-        ->get('role_mapping');
+    $vea = [];
 
-      $vea = [];
+    foreach ($roles as $idx => $rid) {
+      if (
+        \array_key_exists($rid, $role_mapping) &&
+        ! empty($role_mapping[$rid])
+      ) {
+        $key = $role_mapping[$rid];
 
-      foreach ($roles as $idx => $rid) {
-        if (\array_key_exists($rid, $role_mapping)) {
-          $key = $role_mapping[$rid];
-
-          foreach ($sho as $idx => $item) {
-            $vea[] = \implode('@', [$key, $item->value]);
-          }
+        foreach ($sho as $idx => $value) {
+          $vea[] = \implode('@', [$key, $value]);
         }
       }
+    }
 
-      if (! empty($vea)) {
-        // Instantiate our event.
-        $event = new SetUserVopersonExternalAffilliationEvent($uid, $vea);
-        // Dispatch the event.
-        $this->eventDispatcher
-          ->dispatch($event, SetUserVopersonExternalAffilliationEvent::EVENT_NAME);
+    if (! empty($vea)) {
+      $original = $user->get(MyacademicidUserFields::FIELD_VEA)->getValue();
+
+      $user->set(MyacademicidUserFields::FIELD_VEA, $vea);
+
+      $user->_skipProtectedUserFieldConstraint = TRUE;
+      $violations = $user->validate();
+
+      if ($violations->count() > 0) {
+        foreach ($violations as $idx => $violation) {
+          $this->messenger->addError($violation->getMessage());
+        }
+
+        $this->messenger
+          ->addError($this->t('Cannot set %claim claim to %value', [
+            '%claim' => $claim,
+            '%value' => \implode(', ', $value)
+          ]));
+
+        $user->set(MyacademicidUserFields::FIELD_VEA, $original);
       }
     }
   }
@@ -129,69 +163,35 @@ class MyacademicidUserRoles {
   /**
    * Determine roles to assign based on affilliation.
    *
-   * @param string $uid
+   * @param \Drupal\user\UserInterface $user
    * @param array $vea
    */
-  public function rolesFromAffilliation(string $uid, array $vea) {
-    $mode = $this->configFactory
-      ->get('myacademicid_user_fields.settings')
-      ->get('mode');
-
-    if ($mode === MyacademicidUserFields::CLIENT_MODE) {
-      $affilliation_mapping = $this->configFactory
-        ->get('myacademicid_user_roles.affilliation_to_role')
-        ->get('affilliation_mapping');
-
-      $structure = [];
-
-      // Gather all affilliation keys per schac_home_organization.
-      foreach ($vea as $idx => $item) {
-        $parts = \explode('@' ,$item->value);
-        $key = $parts[0];
-        $sho = $parts[1];
-
-        if (\array_key_exists($sho, $structure)) {
-          $structure[$sho][] = $key;
-        }
-        else {
-          $structure[$sho] = [$key];
-        }
-      }
-
-      $roles = [];
-
-      // Gather all mapped roles from affilliation keys.
-      foreach ($structure as $sho => $array) {
-        foreach ($array as $idx => $key) {
-          if (\array_key_exists($key, $affilliation_mapping)) {
-            $roles[] = $affilliation_mapping[$key];
-          }
-        }
-      }
-
-      if (! empty($roles)) {
-        // Instantiate our event.
-        $event = new SetUserRolesEvent($uid, $roles);
-        // Dispatch the event.
-        $this->eventDispatcher
-          ->dispatch($event, SetUserRolesEvent::EVENT_NAME);
-      }
-    }
-  }
-
-  /**
-   * Set roles for a user entity.
-   *
-   * @param string $uid
-   * @param array $roles
-   */
-  public function setUserRoles(string $uid, array $roles) {
+  public function rolesFromAffilliation(UserInterface $user, array $vea) {
     $affilliation_mapping = $this->configFactory
       ->get('myacademicid_user_roles.affilliation_to_role')
       ->get('affilliation_mapping');
 
-    // Current status.
-    $user = User::load($uid);
+    $keys = [];
+    $sho = [];
+
+    // Gather all affilliation keys and schac_home_organization values.
+    foreach ($vea as $idx => $item) {
+      $parts = \explode('@' ,$item);
+      $keys[] = $parts[0];
+      $sho[] = $parts[1];
+    }
+
+    $roles = [];
+
+    // Gather all mapped roles from affilliation keys.
+    foreach ($keys as $idx => $key) {
+      if (
+        \array_key_exists($key, $affilliation_mapping) &&
+        ! empty($affilliation_mapping[$key])
+      ) {
+        $roles[] = $affilliation_mapping[$key];
+      }
+    }
 
     $current = $user->getRoles(TRUE);
 
@@ -204,7 +204,10 @@ class MyacademicidUserRoles {
 
     // Roles to remove.
     foreach ($current as $idx => $rid) {
-      if (! \in_array($rid, $roles) && \in_array($rid, $affilliation_mapping)) {
+      if (
+        ! \in_array($rid, $roles) &&
+        \in_array($rid, $affilliation_mapping)
+      ) {
         $user->removeRole($rid);
       }
     }
