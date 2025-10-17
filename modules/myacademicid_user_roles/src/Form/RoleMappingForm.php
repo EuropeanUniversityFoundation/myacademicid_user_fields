de78591a4cfc68c1fe69a2880179e3389ff1b6b7
Tiago Simões <tiago.simoes@uni-foundation.eu>
2025-10-17 17:33:21 +0000

    Cleans and adds test coverage to config forms in roles submodule

--- modules/myacademicid_user_roles/src/Form/RoleMappingForm.php
+++ modules/myacademicid_user_roles/src/Form/RoleMappingForm.php
@@ -3,6 +3,7 @@
 namespace Drupal\myacademicid_user_roles\Form;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -56,6 +57,8 @@ class RoleMappingForm extends ConfigFormBase {
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The config factory.
+   * @param \Drupal\Core\Config\TypedConfigManagerInterface $typed_config_manager
+   *   The typed configuration manager.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    * @param \Drupal\myacademicid_user_fields\MyacademicidUserAffiliation $affiliation
@@ -67,12 +70,13 @@ class RoleMappingForm extends ConfigFormBase {
    */
   public function __construct(
     ConfigFactoryInterface $config_factory,
+    TypedConfigManagerInterface $typed_config_manager,
     EntityTypeManagerInterface $entity_type_manager,
     MyacademicidUserAffiliation $affiliation,
     MessengerInterface $messenger,
     TranslationInterface $string_translation,
   ) {
-    parent::__construct($config_factory);
+    parent::__construct($config_factory, $typed_config_manager);
     $this->entityTypeManager = $entity_type_manager;
     $this->affiliation       = $affiliation;
     $this->messenger         = $messenger;
@@ -91,6 +95,7 @@ class RoleMappingForm extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
+      $container->get('config.typed'),
       $container->get('entity_type.manager'),
       $container->get('myacademicid_user_fields.affiliation'),
       $container->get('messenger'),
